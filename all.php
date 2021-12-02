f119c63b4b3f19c74d50c229dcb6100fe44b8157
Esben <esben@inkpro.dk>
2021-12-02 23:37:04 +0100

    Added leaderboard to output from all.php

--- all.php
+++ all.php
@@ -9,3 +9,33 @@ foreach($folders as $folder){
         echo "----\n";
     }
 }
+
+if(file_exists(__DIR__.'/.config')){
+    echo "Current score:\n".getLeaderboard()."---\n";
+}
+
+function getLeaderboard() : string
+{
+    $conf = parse_ini_file(__DIR__.'/.config');
+    $curl = curl_init("https://adventofcode.com/2021/leaderboard/private/view/".$conf['LEADERBOARD_ID'].".json");
+    curl_setopt($curl, CURLOPT_COOKIE, "session=".$conf['SESSION_ID']);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $res = curl_exec($curl);
+    try{
+        $data = json_decode($res);
+        $members = (array)$data->members;
+    }catch(\Exception $e){
+        throw new \Exception ("Response from AOC was not json.");
+    }
+    usort($members, function($a, $b){
+        if($b->stars == $a->stars){
+            return $b->local_score <=> $a->local_score;
+        }
+        return $b->stars <=> $a->stars;
+    });
+    $leaderboard = '';
+    foreach($members as $member){
+        $leaderboard .= "{$member->name} - {$member->stars} stars - {$member->local_score} points\n";
+    }
+    return $leaderboard;
+}
